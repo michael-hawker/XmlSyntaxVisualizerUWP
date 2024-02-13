cdb5bb52ec1a08cd4e696bbbb3d10889be74816c
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-02-13 14:10:47 -0800

    Switch from Monaco to Scintilla editor (WinUIEdit.Uwp)
    Notes:
    - CallTips (hover) not working
    - Indicators hard to see in Dark Mode
    - Xml Syntax highlighting needs update to latest version when released
    - Extraneous UpdateUI event calls of type Content when interacting with TreeView

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,22 +2,19 @@
 // Licensed under the MIT License.
 
 using Microsoft.Language.Xml;
-using Monaco;
-using Monaco.Editor;
-using Monaco.Helpers;
-using Monaco.Languages;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
-using Windows.System;
+using System.Xml.Linq;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-
+using WinUIEditor;
 using MUX = Microsoft.UI.Xaml.Controls;
 
 namespace XmlSyntaxVisualizerUwp
@@ -38,14 +35,25 @@ namespace XmlSyntaxVisualizerUwp
         public static readonly DependencyProperty RootNodesProperty =
             DependencyProperty.Register(nameof(RootNodes), typeof(List<XmlSyntaxData>), typeof(MainPage), new PropertyMetadata(new List<XmlSyntaxData>()));
 
-        public Position CurrentPosition
+        public int Column
         {
-            get { return (Position)GetValue(CurrentPositionProperty); }
-            set { SetValue(CurrentPositionProperty, value); }
+            get { return (int)GetValue(ColumnProperty); }
+            set { SetValue(ColumnProperty, value); }
         }
 
-        public static readonly DependencyProperty CurrentPositionProperty =
-            DependencyProperty.Register(nameof(CurrentPosition), typeof(Position), typeof(MainPage), new PropertyMetadata(null));
+        // Using a DependencyProperty as the backing store for Column.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColumnProperty =
+            DependencyProperty.Register(nameof(Column), typeof(int), typeof(MainPage), new PropertyMetadata(0));
+
+        public int LineNumber
+        {
+            get { return (int)GetValue(LineNumberProperty); }
+            set { SetValue(LineNumberProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineNumber.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineNumberProperty =
+            DependencyProperty.Register(nameof(LineNumber), typeof(int), typeof(MainPage), new PropertyMetadata(0));
 
         public string ElementInfo
         {
@@ -63,11 +71,6 @@ namespace XmlSyntaxVisualizerUwp
         #endregion
 
         #region Style Resources
-        private readonly CssLineStyle HighlightStyle = new CssLineStyle()
-        {
-            BackgroundColor = new SolidColorBrush(Colors.Yellow),
-        };
-
         private static readonly SolidColorBrush ListBrush = new SolidColorBrush(Colors.Cyan);
         private static readonly SolidColorBrush TokenBrush = new SolidColorBrush(Colors.LightGreen);
         private static readonly SolidColorBrush SyntaxBrush = new SolidColorBrush(Colors.MediumPurple);
@@ -99,34 +102,54 @@ namespace XmlSyntaxVisualizerUwp
             this.InitializeComponent();
 
             // Register for changes to Editor text or position.
-            XmlEditor.RegisterPropertyChangedCallback(CodeEditor.TextProperty, XmlEditor_TextChanged);
-            XmlEditor.RegisterPropertyChangedCallback(CodeEditor.SelectedRangeProperty, XmlEditor_RangeChanged);
-            
+            XmlEditor.Editor.Modified += Editor_Modified;
+            XmlEditor.Editor.UpdateUI += Editor_UpdateUI;
+            XmlEditor.Editor.MouseDwellTime = 500;
+            XmlEditor.Editor.DwellStart += Editor_DwellStart;
+
             // Load example file
             using (var stream = GetType().Assembly.GetManifestResourceStream("XmlSyntaxVisualizerUwp.ExampleDocument.xml"))
             using (var reader = new StreamReader(stream))
             {
-                XmlEditor.Text = reader.ReadToEnd();
+                XmlEditor.Editor.SetText(reader.ReadToEnd());
+            }
+        }
+
+        private void Editor_Modified(WinUIEditor.Editor sender, WinUIEditor.ModifiedEventArgs args)
+        {
+            if (((ModificationFlags)args.ModificationType).HasFlag(ModificationFlags.InsertText)
+                || ((ModificationFlags)args.ModificationType).HasFlag(ModificationFlags.DeleteText))
+            {
+                // text has changed
+                XmlEditor_TextChanged();
+            }
+        }
+
+        private void Editor_UpdateUI(Editor sender, UpdateUIEventArgs args)
+        {
+            // TODO: Seeing this called when interacting with the TreeView causing the TreeView to scroll awkwardly with the logic we have in UpdateCurrentInfo()...
+            if (((Update)args.Updated).HasFlag(Update.Content)
+                || ((Update)args.Updated).HasFlag(Update.Selection))
+            {
+                // Cursor moved in editor
+                UpdateCurrentInfo(); 
             }
         }
 
         #region Update and Parsing
-        private void XmlEditor_RangeChanged(DependencyObject sender, DependencyProperty dp)
-        {
-            UpdateCurrentInfo(); // Cursor moved in editor
-        }
-
-        private void XmlEditor_TextChanged(DependencyObject sender, DependencyProperty dp)
+        private void XmlEditor_TextChanged()
         {
             // Text changed, we need to clear everything and re-parse
-            XmlEditor.Decorations.Clear();
+            XmlEditor.Editor.IndicatorClearRange(0, XmlEditor.Editor.Length);
 
             // Actually invoke the XmlParser Parser
-            _lastRoot = Parser.ParseText(XmlEditor.Text);
+            _lastRoot = Parser.ParseText(XmlEditor.Editor.GetText(XmlEditor.Editor.Length));
 
             // Translate our parsed tree to our set of UI-ready nodes
-            var list = new List<XmlSyntaxData>();
-            list.Add(XmlSyntaxData.FromNode(_lastRoot));
+            var list = new List<XmlSyntaxData>
+            {
+                XmlSyntaxData.FromNode(_lastRoot)
+            };
             RootNodes = list;
 
             // Update our current location's status
@@ -136,7 +159,7 @@ namespace XmlSyntaxVisualizerUwp
             });
 
             // Update the validation status in the Editor
-            var validRoot = XmlParserHelpers.GetValidXmlTree(XmlEditor.Text);
+            var validRoot = XmlParserHelpers.GetValidXmlTree(XmlEditor.Editor.GetText(XmlEditor.Editor.Length));
 
             ValidXmlDoc.Text = validRoot.ToFullString();
         }
@@ -144,26 +167,21 @@ namespace XmlSyntaxVisualizerUwp
         /// <summary>
         /// This function just provides a bit of info about the Xml Node that's at the caret position in the Editor.
         /// </summary>
-        private async void UpdateCurrentInfo()
+        private void UpdateCurrentInfo()
         {
             // Figure out where we are.
-            CurrentPosition = await XmlEditor.GetPositionAsync();
-
-            if (CurrentPosition == null)
-            {
-                return;
-            }
-
-            // Break this down to convert between the Monaco editor and the index our Xml Parser knows about.
-            var index = XmlEditor.Text.GetCharacterIndex((int)CurrentPosition.LineNumber, (int)CurrentPosition.Column);
+            var index = XmlEditor.Editor.CurrentPos;
 
             if (index == -1)
             {
                 return;
             }
 
+            LineNumber = (int)XmlEditor.Editor.LineFromPosition(index) + 1;
+            Column = (int)XmlEditor.Editor.GetColumn(index) + 1;
+
             // Use the caret position (as index) to find the corresponding Xml Node from our parsed tree.
-            var raw_node = _lastRoot.FindNode(index + 1);
+            var raw_node = _lastRoot.FindNode((int)index);
             var node = XmlSyntaxData.FromNode(raw_node, false); // Translate to our UI-Friendly object
             XmlSyntaxData parent = null;
             if (raw_node.Parent != null)
@@ -176,8 +194,8 @@ namespace XmlSyntaxVisualizerUwp
             if (node != null)
             {
                 // Refetch proper line/col from start of token (as it may start earlier than where the caret is)
-                var (line_s, col_s) = XmlEditor.Text.GetLineColumnIndex(node.SpanStart); // Translate from Xml Parser to Monaco positions.
-                var (line_e, col_e) = XmlEditor.Text.GetLineColumnIndex(node.SpanEnd - 1);
+                //// var (line_s, col_s) = (XmlEditor.Editor.LineFromPosition(node.SpanStart), XmlEditor.Editor.GetColumn(node.SpanStart));
+                //// var (line_e, col_e) = (XmlEditor.Editor.LineFromPosition(node.SpanEnd - 1), XmlEditor.Editor.GetColumn(node.SpanEnd - 1));
 
                 // Provide info in our UI box.
                 ElementInfo = node.Text + Environment.NewLine;
@@ -189,43 +207,33 @@ namespace XmlSyntaxVisualizerUwp
         #endregion
 
         #region Monaco -> TreeView Helpers
-        private async void XmlEditor_Loading(object sender, RoutedEventArgs e)
+        private void Editor_DwellStart(Editor sender, DwellStartEventArgs args)
         {
-            var languages = new Monaco.LanguagesHelper(XmlEditor);
-
-            await languages.RegisterHoverProviderAsync("xml", (model, position) =>
+            // Figure out where we our in relation to our Xml Tree
+            var index = args.Position;
+            
+            if (index == -1)
             {
-                return AsyncInfo.Run(async delegate (CancellationToken cancelationToken)
-                {
-                    // Figure out where we our in relation to our Xml Tree
-                    var index = XmlEditor.Text.GetCharacterIndex((int)position.LineNumber, (int)position.Column);
+                return;
+            }
 
-                    if (index == -1)
-                    {
-                        return default;
-                    }
+            // Get that node from the Xml Parser and wrap it in a friendly container.
+            var node = XmlSyntaxData.FromNode(_lastRoot.FindNode(index + 1), false);
 
-                    // Get that node from the Xml Parser and wrap it in a friendly container.
-                    var node = XmlSyntaxData.FromNode(_lastRoot.FindNode(index + 1), false);
+            if (node != null)
+            {
+                // Refetch proper line/col from start of token
+                var (line_s, col_s) = (XmlEditor.Editor.LineFromPosition(node.SpanStart), XmlEditor.Editor.GetColumn(node.SpanStart));
+                //// var (line_e, col_e) = (XmlEditor.Editor.LineFromPosition(node.SpanEnd - 1), XmlEditor.Editor.GetColumn(node.SpanEnd - 1));
 
-                    if (node != null)
-                    {
-                        // Refetch proper line/col from start of token
-                        var (line_s, col_s) = XmlEditor.Text.GetLineColumnIndex(node.SpanStart);
-                        var (line_e, col_e) = XmlEditor.Text.GetLineColumnIndex(node.SpanEnd - 1);
+                // TODO: This doesn't seem to be working/displaying?
+                // Provide nice UI on hover to show more info about the node.
+                XmlEditor.Editor.CallTipShow(index, 
+                                             "*" + node.Type + "* " + node.Text + " [" + node.SpanStart + ".." + node.SpanEnd + ")\n" +
+                                             "Line: " + line_s + " Col: " + col_s + " Length: " + node.Length);
+            }
 
-                        // Provide nice UI on hover to show more info about the node.
-                        return new Hover(new string[]
-                            {
-                                "*" + node.Type + "* " + node.Text + " [" + node.SpanStart + ".." + node.SpanEnd + ")",
-                                "Line: " + line_s + " Col: " + col_s + " Length: " + node.Length
-                            }, 
-                            new Range((uint)line_s, (uint)col_s, (uint)line_e, (uint)col_e + 1));
-                    }
-
-                    return default;
-                });
-            });
+            return;
         }
 
         private void TreeView_ScrollNode(SyntaxNode raw_node)
@@ -277,29 +285,29 @@ namespace XmlSyntaxVisualizerUwp
             {
                 _lastData = data; // Cache so we only update Editor on initial mouse change
 
-                var (line_s, col_s) = XmlEditor.Text.GetLineColumnIndex(data.SpanStart);
-                var (line_e, col_e) = XmlEditor.Text.GetLineColumnIndex(data.SpanEnd - 1);
+                ////var (line_s, col_s) = (XmlEditor.Editor.LineFromPosition(data.SpanStart), XmlEditor.Editor.GetColumn(data.SpanStart));
+                ////var (line_e, col_e) = (XmlEditor.Editor.LineFromPosition(data.SpanEnd - 1), XmlEditor.Editor.GetColumn(data.SpanEnd - 1));
 
-                XmlEditor.Decorations.Clear();
-                
-                XmlEditor.Decorations.Add(
-                    new IModelDeltaDecoration(new Range((uint)line_s, (uint)col_s, (uint)line_e, (uint)(col_e + 1)), new IModelDecorationOptions()
-                {
-                    ClassName = HighlightStyle,
-                    Stickiness = TrackedRangeStickiness.AlwaysGrowsWhenTypingAtEdges
-                }));
+                XmlEditor.Editor.IndicatorClearRange(0, XmlEditor.Editor.Length);
+
+                XmlEditor.Editor.IndicatorCurrent = 0;
+                XmlEditor.Editor.IndicSetStyle(0, IndicatorStyle.FullBox);
+                XmlEditor.Editor.IndicatorFillRange(data.SpanStart, data.SpanEnd - data.SpanStart);
+
+                ////ClassName = HighlightStyle,
+                ////Stickiness = TrackedRangeStickiness.AlwaysGrowsWhenTypingAtEdges
             }
         }
 
-        private async void XmlSyntaxTree_ItemInvoked(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewItemInvokedEventArgs args)
+        private void XmlSyntaxTree_ItemInvoked(Microsoft.UI.Xaml.Controls.TreeView sender, Microsoft.UI.Xaml.Controls.TreeViewItemInvokedEventArgs args)
         {
             // If we click on the TreeViewItem we've moused over, then move the caret to that location
             // to update our UI info box.
             if (_lastData != null)
             {
-                var (line_s, col_s) = XmlEditor.Text.GetLineColumnIndex(_lastData.SpanStart);
+                var line_s = XmlEditor.Editor.LineFromPosition(_lastData.SpanStart);
 
-                await XmlEditor.SetPositionAsync(new Position((uint)line_s, (uint)col_s));
+                XmlEditor.Editor.FirstVisibleLine = Math.Max(line_s - XmlEditor.Editor.LinesOnScreen / 2, 0);
             }
         }
         #endregion
