5fec6fc326685c85fed7bc89c3972f01c5c15e94
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2025-11-22 17:12:08 -0800

    [WIP] Scaffold plans?

---
+++ XmlSyntax.Models/XmlDocumentSyntaxExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Language.Xml;
+
+namespace XmlSyntax.Models;
+
+public static class XmlDocumentSyntaxExtensions
+{
+    public static Range[] GetErrorRanges(this XmlDocumentSyntax tree)
+    {
+        // TODO: Create own Range-type struct
+        // Walk tree and be smart about detecting the error locations and mismatched pairs.
+        // Also get start/end, so middle can be OK still... (work through scenarios and expectations)
+    }
+
+    // TODO: Create another string extension helper that takes the set of ranges and returns the modified string, use that to get the expected output.
+    // TODO: Have a tree pruner that can return a tree without the specified ranges? Needs to be able re-parent, etc...?
+}
